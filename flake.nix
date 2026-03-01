4f3bbb9a01b491f3fb49228b57888d7d678e4759
Li Kai <hello+github@lik.ai>
2026-03-02 03:22:23 +0800

    Release v0.1.5

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
       {
         packages.default = rustPlatform.buildRustPackage {
           pname = "treetok";
-          version = "0.1.4";
+          version = "0.1.5";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
           cargoBuildFlags = [ "--package" "treetok" ];
